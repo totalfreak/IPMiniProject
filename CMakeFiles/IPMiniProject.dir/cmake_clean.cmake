52e25c139816af348640eb9c35e61cfec2de2684
Daniel <dgkartin@live.dk>
2017-10-19 20:53:01 +0200

    Made project

---
+++ CMakeFiles/IPMiniProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IPMiniProject.dir/main.cpp.o"
+  "IPMiniProject.pdb"
+  "IPMiniProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IPMiniProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/IPMiniProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for IPMiniProject.
+# This may be replaced when dependencies are built.
